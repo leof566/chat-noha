92aeefbc7748fc3e66dd559afded7a1d0d304ac7
leonardo diaz <leonardodiaz3438@gmail.com>
2025-08-31 21:01:49 -0300

    manifest.webmanifest.js

--- manifest.webmanifest.js
+++ manifest.webmanifest.js
@@ -1,13 +1,26 @@
 {
   "name": "Chat Noha",
   "short_name": "ChatNoha",
+  "id": "chat-noha",
+  "description": "Chat en línea sencillo con Firebase",
   "start_url": "/chat-noha/",
   "scope": "/chat-noha/",
   "display": "standalone",
   "background_color": "#000000",
   "theme_color": "#000000",
+  "launch_handler": {
+    "client_mode": "navigate-existing"
+  },
   "icons": [
-    { "src": "icons/icon-192.png", "sizes": "192x192", "type": "image/png" },
-    { "src": "icons/icon-512.png", "sizes": "512x512", "type": "image/png" }
+    {
+      "src": "icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
   ]
 }
